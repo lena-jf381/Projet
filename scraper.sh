2db7efdadf2d919499a778794488b8161a7742af
Hugo Fagnoni <fagnoni.hugo@gmail.com>
2025-03-19 17:47:28 +0100

    Résolution Bug

--- scraper.sh
+++ scraper.sh
@@ -2,6 +2,6 @@
 
 url="https://wise.com/fr/stock/aapl"
 
-PRICE=$(curl -s $URL | grep -oP '(?<=<div class="mw-display-3 m-b-1">)[^<]+' | head -n 1)
+price=$(curl -s $url | grep -oP '(?<=<div class="mw-display-3 m-b-1">)[^<]+' | head -n 1)
 
-echo "Nombre total de cas : $PRICE"
+echo "$price"

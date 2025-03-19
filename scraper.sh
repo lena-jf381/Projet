408ef0ca9a2a96204ea610710d41e7f4c61a7674
Hugo Fagnoni <fagnoni.hugo@gmail.com>
2025-03-19 15:54:43 +0100

    Modification du script de scraping

--- scraper.sh
+++ scraper.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# URL du site à scraper
 url="https://www.worldometers.info/coronavirus/"
 
-# Scraping pour obtenir le nombre de cas totaux
 cas=$(curl -s $url | grep -oP '(?<=<span style="color:#aaa">)[0-9,]+' | head -n 1)
-
 
 echo "Nombre total de cas : $cas"

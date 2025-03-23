6950b700ed259a90bc17915a086dedb02ffe9391
Léna <lena.jeanfrancois@outlook.com>
2025-03-23 15:27:34 +0100

    Mise à jourr scrapper

--- scraper.sh
+++ scraper.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-url="https://wise.com/fr/stock/aapl"
+url="https://wise.com/fr/stock/"
 
-price=$(curl -s $url | grep -oP '(?<=<div class="mw-display-3 m-b-1">)[^<]+' | head -n 1)
+get_valeur(){
+    price=$(curl -s $url$2 | grep -oP '(?<=<div class="mw-display-3 m-b-1">)[^<]+' | head -n 1)
+    #Récupère la date et le prix. Sauvegarde dans un fichier avec le nom de l'entreprise en minuscule 
+    echo "$(date '+%d/%m/%Y %H:%M:%S');$price" >> data_$(echo $1 |  tr '[A-Z]' '[a-z]').csv
+}
 
-echo "$(date '+%d/%m/%Y %H:%M:%S');$price" >> data.csv
+get_valeur "APPLE" "aapl"
+get_valeur "MICROSOFT" "msft"
+get_valeur "AMAZON" "amzn"
+get_valeur "Google" "googl"

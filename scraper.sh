e0a8e2f681bcead00ce325b734f0aaa80692af55
Léna <lena.jeanfrancois@outlook.com>
2025-03-23 12:39:56 +0100

    Mise à jour scrapper

--- scraper.sh
+++ scraper.sh
@@ -4,4 +4,4 @@ url="https://wise.com/fr/stock/aapl"
 
 price=$(curl -s $url | grep -oP '(?<=<div class="mw-display-3 m-b-1">)[^<]+' | head -n 1)
 
-echo "$price"
+echo "$(date '+%d/%m/%Y %H:%M:%S');$price" >> data.csv
